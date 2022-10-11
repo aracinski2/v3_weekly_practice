81f8b2dab2dc047ca81e328820ce32232c345492
Alex Racinski <aracinski2@gmail.com>
2022-10-10 19:24:07 -0500

    conditionals weekly practice problem 3

--- conditionals_practice_probs.rb
+++ conditionals_practice_probs.rb
@@ -7,11 +7,20 @@
 # end
 
 # 2
-n = 11
-if n < 10
-  p -1
-elsif n > 10
+# n = 11
+# if n < 10
+#   p -1
+# elsif n > 10
+#   p 1
+# else
+#   p 0
+# end
+
+# 3
+n1 = 3
+n2 = 11
+if n1 < 10 && n2 < 10
   p 1
 else
-  p 0
+  p 0 
 end
