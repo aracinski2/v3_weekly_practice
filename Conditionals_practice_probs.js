db42289cbb4fa32f94f0a76fe0c627ac41422b8b
Alex Racinski <aracinski2@gmail.com>
2023-03-19 17:31:35 -0500

    javascript conditional weekly practice problem 2

--- Conditionals_practice_probs.js
+++ Conditionals_practice_probs.js
@@ -1,8 +1,19 @@
 // 1
-let x = 12;
+// let x = 12;
 
-if (x === 10) {
-  console.log(0);
-} else {
+// if (x === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
+
+// 2
+let number = 12;
+
+if (number < 10) {
   console.log(-1);
+} else if (number > 10) {
+  console.log(1);
+} else {
+  console.log(0);
 }
