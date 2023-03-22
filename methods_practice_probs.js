7d56a3991383968ce5ff81202acebbe55cadccdf
Alex Racinski <aracinski2@gmail.com>
2023-03-22 16:22:49 -0500

    javascript methods practice problem 2

--- methods_practice_probs.js
+++ methods_practice_probs.js
@@ -1,4 +1,11 @@
 // 1
+// const prompt = require("prompt-sync")({ sigint: true });
+// let word = prompt("Please enter a word");
+// console.log(word.toUpperCase());
+
+// 2
 const prompt = require("prompt-sync")({ sigint: true });
-let word = prompt("Please enter a word");
-console.log(word.toUpperCase());
+let number = prompt("Please enter a number:")
+if (parseInt(number) > 100) {
+  console.log("That's a big number");
+}
