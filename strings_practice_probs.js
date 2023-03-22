076c1b7cd19649c5438c9c82af5576aa9f4302d0
Alex Racinski <aracinski2@gmail.com>
2023-03-22 15:39:50 -0500

    javascript strings practice problem 3

--- strings_practice_probs.js
+++ strings_practice_probs.js
@@ -5,6 +5,13 @@
 // console.log(fname + " " + lname)
 
 // 2
-let fname = "billy"
-let lname = "bob"
-console.group(`${fname} ${lname}`)
+// let fname = "billy"
+// let lname = "bob"
+// console.group(`${fname} ${lname}`)
+
+// 3
+const prompt = require("prompt-sync")({ sigint: true });
+var word = prompt("Please input a word");
+if (word === "marco") {
+  console.log("polo")
+}
