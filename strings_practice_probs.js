24293baa3344e6985bfbbe492bfff10d4049892c
Alex Racinski <aracinski2@gmail.com>
2023-03-22 15:43:59 -0500

    javascript strings practice problem 4

--- strings_practice_probs.js
+++ strings_practice_probs.js
@@ -10,8 +10,14 @@
 // console.group(`${fname} ${lname}`)
 
 // 3
-const prompt = require("prompt-sync")({ sigint: true });
-var word = prompt("Please input a word");
-if (word === "marco") {
-  console.log("polo")
-}
+// const prompt = require("prompt-sync")({ sigint: true });
+// var word = prompt("Please input a word");
+// if (word === "marco") {
+//   console.log("polo")
+// }
+
+// 4
+let color1 = "blue";
+let color2 = "red";
+let color3 = "green";
+console.log("My favorite colors are " + color1 + ", " + color2 + ", " + color3 + "!")
