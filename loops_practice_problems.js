275f93dabf93237f9f3886c339257c0c924d0bea
Alex Racinski <aracinski2@gmail.com>
2023-03-23 13:45:52 -0500

    javascript loops practice problem 2

--- loops_practice_problems.js
+++ loops_practice_problems.js
@@ -1,6 +1,13 @@
 // 1
-let number = 1;
-while (number <= 10) {
-  console.log(number);
-  ++number;
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   ++number;
+// }
+
+// 2
+let i = 1;
+while (i <= 5) {
+  console.log("hello");
+  i++;
 }
