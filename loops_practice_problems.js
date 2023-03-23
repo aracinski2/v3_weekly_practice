c3125ae84ea3be279c2278f314b12ac318359940
Alex Racinski <aracinski2@gmail.com>
2023-03-23 14:53:49 -0500

    javascript loops practice problem 3

--- loops_practice_problems.js
+++ loops_practice_problems.js
@@ -6,8 +6,17 @@
 // }
 
 // 2
-let i = 1;
-while (i <= 5) {
-  console.log("hello");
-  i++;
+// let i = 1;
+// while (i <= 5) {
+//   console.log("hello");
+//   i++;
+// }
+
+// 3
+const prompt = require("prompt-sync")({ sigint: true });
+while (true) {
+  word = prompt("Please enter a word: ");
+  if (word === "stop") {
+    break;
+  }
 }
