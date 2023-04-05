39ab70324ec97c256a3c939176432845ab2592ff
Alex Racinski <aracinski2@gmail.com>
2023-04-05 14:13:18 -0500

    javascript hash practice problem 2

--- hash_practice_problems.js
+++ hash_practice_problems.js
@@ -1,9 +1,14 @@
 // 1
-let person = {
-  first_name: "Billy",
-  last_name: "Bob",
-  email: "billybob@gmail"
-};
-console.log(person.first_name);
-console.log(person.last_name);
-console.log(person.email);
+// let person = {
+//   first_name: "Billy",
+//   last_name: "Bob",
+//   email: "billybob@gmail"
+// };
+// console.log(person.first_name);
+// console.log(person.last_name);
+// console.log(person.email);
+
+// 2
+let people = [{ first_name: "James", last_name: "Bond" }, { first_name: "Indiana", last_name: "Jones" }, { first_name: "Jon", last_name: "Snow" }];
+console.log(people[0]);
+console.log(people[0].first_name + " " + people[0].last_name);
