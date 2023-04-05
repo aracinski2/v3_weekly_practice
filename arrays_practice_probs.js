1f163e9a1359c696631fab70febc630fa2871704
Alex Racinski <aracinski2@gmail.com>
2023-04-05 13:08:20 -0500

    javascript array practice problem 3

--- arrays_practice_probs.js
+++ arrays_practice_probs.js
@@ -5,6 +5,15 @@
 // console.log(array);
 
 // 2
-let letters = ["b", "i", "k", "e"];
-letters[1] = 3;
-console.log(letters);
+// let letters = ["b", "i", "k", "e"];
+// letters[1] = 3;
+// console.log(letters);
+
+// 3
+let numbers = [1, 3, 5, 7, 9]
+// numbers.forEach(function (number) {
+//   console.log(number)
+// });
+for (i = 0; i < 5; i++) {
+  console.log(numbers[i]);
+}
