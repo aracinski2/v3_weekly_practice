f7d24f3c3dc1a76bca4606cb68d647d4515c9c10
Alex Racinski <aracinski2@gmail.com>
2023-04-05 14:09:01 -0500

    javascript hash practice problem 1

---
+++ hash_practice_problems.js
@@ -0,0 +1,9 @@
+// 1
+let person = {
+  first_name: "Billy",
+  last_name: "Bob",
+  email: "billybob@gmail"
+};
+console.log(person.first_name);
+console.log(person.last_name);
+console.log(person.email);
