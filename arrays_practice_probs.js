46a6139fdcd9bd133dbc9d1a539ae864b71f8cf6
Alex Racinski <aracinski2@gmail.com>
2023-04-05 13:02:38 -0500

    javascript array practice problem 2

--- arrays_practice_probs.js
+++ arrays_practice_probs.js
@@ -1,5 +1,10 @@
 // 1
-let array = ["red", "blue", "green"];
-array.push("pink");
-array.push("purple");
-console.log(array);
+// let array = ["red", "blue", "green"];
+// array.push("pink");
+// array.push("purple");
+// console.log(array);
+
+// 2
+let letters = ["b", "i", "k", "e"];
+letters[1] = 3;
+console.log(letters);
