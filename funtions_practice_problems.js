55012360030b8d93b1d974b11a10ca54a5214fc8
Alex Racinski <aracinski2@gmail.com>
2023-04-06 15:24:19 -0500

    javascript functions practice problem 2

--- funtions_practice_problems.js
+++ funtions_practice_problems.js
@@ -1,5 +1,11 @@
 // 1
-function timesTwo(x) {
-  return x * 2;
+// function timesTwo(x) {
+//   return x * 2;
+// }
+// console.log(timesTwo(8));
+
+// 2
+function capitalString(word) {
+  return word.toUpperCase();
 }
-console.log(timesTwo(8));
+console.log(capitalString("fries"));
