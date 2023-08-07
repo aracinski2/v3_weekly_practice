18db3a2d0b2cb53532b9606902751c5e5251f16e
Alex Racinski <aracinski2@gmail.com>
2023-08-07 17:30:28 -0500

    arrays map practice problems while, forEach, .map number 2

--- arrays_map_practice_problems.js
+++ arrays_map_practice_problems.js
@@ -1,35 +1,51 @@
 // 1
-let array = [1, 2, 3]
-// while loop
-let i = 0
-let newArray = [];
-while (i < array.length) {
-  newArray.push(array[i] * 3);
-  i++
-}
-console.log(newArray);
-// forEach
-let newArray2 = [];
-// for (i = 0; i < array.length; i++) {
-//   newArray2.push(array[i] * 3);
+// let array = [1, 2, 3]
+// // while loop
+// let i = 0
+// let newArray = [];
+// while (i < array.length) {
+//   newArray.push(array[i] * 3);
+//   i++
 // }
-array.forEach(function (number) {
-  newArray2.push(number * 3);
-});
-console.log(newArray2);
-// .map
-let newArray3 = [];
-array.map(function (number) {
-  newArray3.push(number * 3);
-});
-console.log(newArray3);
+// console.log(newArray);
+// // forEach
+// let newArray2 = [];
+// // for (i = 0; i < array.length; i++) {
+// //   newArray2.push(array[i] * 3);
+// // }
+// array.forEach(function (number) {
+//   newArray2.push(number * 3);
+// });
+// console.log(newArray2);
+// // .map
+// let newArray3 = [];
+// array.map(function (number) {
+//   newArray3.push(number * 3);
+// });
+// console.log(newArray3);
 
 // 2
+let strings = ["red", "blue", "green"];
 // while loop
+let upStrings = [];
+let i = 0;
+while (i < strings.length) {
+  upStrings.push(strings[i].toUpperCase());
+  i++;
+}
+console.log(upStrings);
 
 // for each
-
-// .map 
+let upStrings2 = [];
+strings.forEach(function (string) {
+  upStrings2.push(string.toUpperCase());
+})
+console.log(upStrings2);
+// .map
+let upStrings3 = strings.map(function (string) {
+  return string.toUpperCase();
+})
+console.log(upStrings3);
 
 // 3
 // while loop
