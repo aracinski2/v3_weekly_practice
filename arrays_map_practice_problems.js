7c742dfe0787f3fb407653acd4934820c5da9d49
Alex Racinski <aracinski2@gmail.com>
2023-08-07 17:20:33 -0500

    arrays map practice problems while, forEach, .map number 1

---
+++ arrays_map_practice_problems.js
@@ -0,0 +1,88 @@
+// 1
+let array = [1, 2, 3]
+// while loop
+let i = 0
+let newArray = [];
+while (i < array.length) {
+  newArray.push(array[i] * 3);
+  i++
+}
+console.log(newArray);
+// forEach
+let newArray2 = [];
+// for (i = 0; i < array.length; i++) {
+//   newArray2.push(array[i] * 3);
+// }
+array.forEach(function (number) {
+  newArray2.push(number * 3);
+});
+console.log(newArray2);
+// .map
+let newArray3 = [];
+array.map(function (number) {
+  newArray3.push(number * 3);
+});
+console.log(newArray3);
+
+// 2
+// while loop
+
+// for each
+
+// .map 
+
+// 3
+// while loop
+
+// for each
+
+// .map 
+
+// 4
+// while loop
+
+// for each
+
+// .map 
+
+// 5
+// while loop
+
+// for each
+
+// .map 
+
+// 6
+// while loop
+
+// for each
+
+// .map 
+
+// 7
+// while loop
+
+// for each
+
+// .map 
+
+// 8
+// while loop
+
+// for each
+
+// .map 
+
+// 9
+// while loop
+
+// for each
+
+// .map 
+
+// 10
+// while loop
+
+// for each
+
+// .map 
